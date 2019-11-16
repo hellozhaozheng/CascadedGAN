ff7825eb3d67a38e5cc66b3dadb47aba563fbbf8
ksws840662044 <hellozhaozheng@foxmail.com>
2019-11-16 12:30:34 +0800

    update test script

--- test.py
+++ test.py
@@ -1,22 +1,17 @@
-"""
-Copyright (C) 2019 NVIDIA Corporation.  All rights reserved.
-Licensed under the CC BY-NC-SA 4.0 license (https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode).
-"""
 
 import os
 from collections import OrderedDict
 
 import data
-from options.test_options import TestOptions
-from models.pix2pix_model import Pix2PixModel
-from util.visualizer import Visualizer
-from util import html
+from cfgs.test import Options
+from models.model import Model
+from tools import html
 
-opt = TestOptions().parse()
+opt = Options().parse()
 
 dataloader = data.create_dataloader(opt)
 
-model = Pix2PixModel(opt)
+model = Model(opt)
 model.eval()
 
 visualizer = Visualizer(opt)
